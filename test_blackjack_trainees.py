76e6f76fe8bd4a0d88538e3d71ec720745d129ee
Dan Keefe <peritract@hotmail.co.uk>
2023-06-06 11:43:56 +0100

    Update test_blackjack_trainees.py
    Remove manual output capture

--- test_blackjack_trainees.py
+++ test_blackjack_trainees.py
@@ -1,2 +1,2 @@
 from blackjack import generate_deck, points_for, play
-from support.testing_util import player_chooses, reset_test_suite, capture_print_lines
+from support.testing_util import player_chooses
