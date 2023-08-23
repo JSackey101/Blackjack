81827470514d7cefd0afbf8ad7bc9251042e8b37
Chris Owen <chris.owen@sigmalabs.co.uk>
2023-08-23 15:22:29 +0100

    Add more challenges

--- test_blackjack_trainees.py
+++ test_blackjack_trainees.py
@@ -1,2 +1,4 @@
-from blackjack import generate_deck, points_for, play
+"""File for tests written by you - the trainee"""
+
+from blackjack import generate_deck, points_for, play, get_next_card_from_deck, deal_card_to_hand
 from support.testing_util import player_chooses
