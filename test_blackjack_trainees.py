1d5cb49e799d55ef78ac7cbedd84eea1c4c4eea9
JSackey101 <jeffsackey101@gmail.com>
2025-01-07 16:01:41 +0000

    Added tests for the points_for function

--- test_blackjack_trainees.py
+++ test_blackjack_trainees.py
@@ -12,3 +12,16 @@ def test_generate_deck():
                                "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AC", "2C", "3C", "4C",
                                "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AH", "2H", "3H", "4H",
                                "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH"]
+
+
+def test_points_for():
+    assert points_for(["7H", "2D"]) == 9
+
+
+def test_two_aces():
+    assert points_for(["AC", "AS"]) == 21
+
+
+def test_hand_over_five():
+    assert points_for(["1C", "2S", "3S", "4S",
+                       "1H", "2C"]) == 21
